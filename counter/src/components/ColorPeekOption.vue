da1b05384b7f9e72d3e86b9008b44a488fc54bea
seona-shim <hogugu4102@codebrick.co>
2022-04-26 16:17:24 +0900

    Add color option component

---
+++ counter/src/components/ColorPeekOption.vue
@@ -0,0 +1,27 @@
+<template>
+  <select name="color" @change="changeSelect">
+    <option value="blue" :selected="selectedOption('low')">blue</option>
+    <option value="red" :selected="selectedOption('high')">red</option>
+    <option value="green" :selected="false">green</option>
+    <option value="black" :selected="selectedOption('middle')">black</option>
+  </select>
+</template>
+<script>
+export default {
+  name: "ColorPeekOption",
+  props: {
+    selectType: String,
+  },
+  methods: {
+    changeSelect(e) {
+      this.$store.commit("updateResultColor", {
+        type: this.selectType,
+        value: e.target.value,
+      });
+    },
+    selectedOption(type) {
+      return this.selectType == type;
+    },
+  },
+};
+</script>
