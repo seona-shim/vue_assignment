4baf6d85e1ff44509513bf3406b292206790a6d1
seona-shim <hogugu4102@codebrick.co>
2022-04-29 19:03:20 +0900

    Add test plugin file

--- counter/src/main.js
+++ counter/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
+import testPlugin from "./plugins/TestPlugin";
 
-createApp(App).use(store).mount("#app");
+const app = createApp(App);
+
+app.use(store);
+app.use(testPlugin);
+app.mount("#app");
