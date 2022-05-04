b0041ca2c93403dea93880eec4ac858515319a8c
seona-shim <hogugu4102@codebrick.co>
2022-05-04 18:06:38 +0900

    Add color pick event

--- counter-pinia/src/components/colorPicker/ColorPickerBox.vue
+++ counter-pinia/src/components/colorPicker/ColorPickerBox.vue
@@ -2,19 +2,28 @@
   <section class="color-picker-box">
     <label>
       <h3>? > 100</h3>
-      <element-select></element-select>
+      <element-select
+        :value="resultColors.high"
+        @change="colorStore.changeColor('high', $event.target.value)"
+      ></element-select>
     </label>
     <label>
       <h3>0 {{ "<=" }} ? {{ "<=" }} 100</h3>
-      <element-select></element-select>
+      <element-select
+        :value="resultColors.middle"
+        @change="colorStore.changeColor('middle', $event.target.value)"
+      ></element-select>
     </label>
     <label>
       <h3>? {{ "<" }} 0</h3>
-      <element-select></element-select>
+      <element-select
+        :value="resultColors.low"
+        @change="colorStore.changeColor('low', $event.target.value)"
+      ></element-select>
     </label>
   </section>
 </template>
-<script>
+<script lang="ts">
 import { defineComponent } from "vue";
 import ElementSelect from "../elements/ElementSelect.vue";
 
@@ -23,7 +32,19 @@ export default defineComponent({
   name: "ColorPickerBox",
 });
 </script>
-<script setup></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { useColorStore } from "../../store/color";
+const colorStore = useColorStore();
+
+export interface Colors {
+  high: string;
+  middle: string;
+  low: string;
+}
+
+const resultColors = ref<Colors>({ high: "red", middle: "black", low: "blue" });
+</script>
 <style lang="scss" scoped>
 .color-picker-box {
   justify-content: space-between;
--- counter-pinia/src/components/elements/ElementSelect.vue
+++ counter-pinia/src/components/elements/ElementSelect.vue
@@ -1,22 +1,35 @@
 <template>
-  <select class="select-color">
+  <select
+    class="select-color"
+    v-model="selectBackground"
+    :style="{ background: selectBackground }"
+  >
     <option>black</option>
     <option>green</option>
     <option>red</option>
     <option>blue</option>
   </select>
 </template>
-<script>
+<script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "ElementSelect",
 });
 </script>
-<script setup></script>
+<script setup lang="ts">
+import { ref, defineProps } from "vue";
+
+const props = defineProps<{
+  value: string;
+}>();
+
+const selectBackground = ref<string>(props.value);
+</script>
 <style lang="scss" scoped>
 .select-color {
   width: 100%;
   padding: 10px 20px;
+  color: white;
 }
 </style>
