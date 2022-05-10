a744c502a2b3db2ee1e85fdb522578cf790d7326
seona-shim <hogugu4102@codebrick.co>
2022-05-10 12:37:43 +0900

    Add counter feature and style

--- counter-pinia/src/components/tailwind/CounterBox.vue
+++ counter-pinia/src/components/tailwind/CounterBox.vue
@@ -6,19 +6,23 @@
     <input
       type="number"
       class="border-black-500 h-[40px] w-[300px] border-2 bg-gray-100"
+      v-model="countValue"
     />
     <div class="flex justify-center gap-[10px]">
       <button
+        @click="changeCount('plus')"
         class="h-[40px] w-[50px] rounded-lg bg-red-500 text-lg font-bold text-white active:bg-red-700"
       >
         +
       </button>
       <button
+        @click="changeCount('minus')"
         class="h-[40px] w-[50px] rounded-lg bg-blue-500 text-lg font-bold text-white active:bg-blue-700"
       >
         -
       </button>
       <button
+        @click="changeCount('random')"
         class="h-[40px] w-[100px] rounded-lg bg-purple-500 text-lg font-bold text-white active:bg-purple-700"
       >
         Random
@@ -32,7 +36,17 @@ export default {
   components: {},
 };
 </script>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from "vue";
+import { useCounterStore } from "@/store";
+const counterStore = useCounterStore();
+
+const countValue = ref<number>(0);
+
+const changeCount = (type: string) => {
+  counterStore.changeCount(type, countValue.value);
+};
+</script>
 <style scoped src="@/styles/tailwind/index.css" />
 
 <style lang="scss" scoped></style>
---
+++ counter-pinia/src/components/tailwind/ResultBox.vue
@@ -0,0 +1,33 @@
+<template>
+  <section
+    class="flex flex-col flex-nowrap items-center justify-center gap-[10px] overflow-auto shadow-lg"
+  >
+    <p v-for="(item, index) in counterStore.result" :key="index">
+      {{ item.math }}
+      <span :class="`text-${getResultColor(item.result)}`" class="font-bold">
+        {{ item.result }}
+      </span>
+    </p>
+  </section>
+</template>
+<script lang="ts">
+export default {
+  name: "ResultBox",
+};
+</script>
+<script lang="ts" setup>
+import { computed } from "vue";
+import { useCounterStore, useColorStore } from "@/store";
+import { getResultType } from "@/methods/getResultType";
+const counterStore = useCounterStore();
+const colorStore = useColorStore();
+
+const getResultColor = (result: number) => {
+  return colorStore.color[getResultType(result)];
+};
+
+const backgroundColor = computed(() => colorStore.background);
+</script>
+<style scoped src="@/styles/tailwind/index.css" />
+
+<style lang="scss" scoped></style>
