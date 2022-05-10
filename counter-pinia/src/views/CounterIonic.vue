11b2dc4128001c0118465773624837f4ba3496c6
seona-shim <hogugu4102@codebrick.co>
2022-05-10 18:59:28 +0900

    Add ui framework selector

--- counter-pinia/src/views/CounterIonic.vue
+++ counter-pinia/src/views/CounterIonic.vue
@@ -1,20 +1,91 @@
 <template>
-  <ion-app>
-    <ion-content>
-      <ion-card>option</ion-card>
-      <ion-card>counter</ion-card>
-      <ion-card>result</ion-card>
-    </ion-content>
-  </ion-app>
+  <ion-content>
+    <main class="contents">
+      <ion-card class="option-box">
+        <ion-item>
+          <ion-select
+            placeholder="ionic"
+            interface="action-sheet"
+            @ionChange="changeUI"
+          >
+            <ion-select-option
+              v-for="(item, index) in uiComponent"
+              :key="index"
+              :value="item"
+              >{{ item }}</ion-select-option
+            >
+          </ion-select>
+        </ion-item>
+        <ion-list></ion-list>
+      </ion-card>
+      <ion-card class="counter-box">counter</ion-card>
+      <ion-card class="result-box">result</ion-card>
+    </main>
+  </ion-content>
 </template>
 <script lang="ts">
-import { IonApp, IonContent, IonCard } from "@ionic/vue";
+import {
+  IonContent,
+  IonCard,
+  IonSelect,
+  IonItem,
+  IonList,
+  IonSelectOption,
+} from "@ionic/vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "CounterIonic",
-  components: { IonApp, IonContent, IonCard },
+  components: {
+    IonContent,
+    IonCard,
+    IonSelect,
+    IonItem,
+    IonList,
+    IonSelectOption,
+  },
 });
 </script>
 
-<style lang="scss"></style>
+<script setup lang="ts">
+import { ref, watch } from "vue";
+import { useColorStore } from "@/store";
+import { Colors, ColorArray } from "@/types/color";
+import useChangeView from "@/methods/useChangeView.ts";
+import { uiComponent } from "@/router/routePath.ts";
+
+const uiValue = ref<string>("ionic");
+
+const changeUI = (e) => {
+  useChangeView(e.target.value);
+};
+
+const colorArray: ColorArray = ["black", "green", "red", "blue"];
+const colorStore = useColorStore();
+
+const resultColors = ref<Colors>({ high: "red", middle: "black", low: "blue" });
+
+const backgroundColor = ref<string>("#ffffff");
+watch(backgroundColor, (newValue, oldValue) => {
+  colorStore.changeBackground(newValue);
+});
+</script>
+
+<style lang="scss" scoped>
+.contents {
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: auto 1fr;
+  gap: 20px;
+  padding: 20px;
+  box-sizing: border-box;
+  .option-box {
+    grid-column: 1/3;
+  }
+  .counter-box {
+  }
+  .result-box {
+  }
+}
+</style>
