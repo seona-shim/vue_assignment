185e5fdc715348bdb11279f5b8acf3fc2471a10f
seona-shim <hogugu4102@codebrick.co>
2022-05-17 15:52:38 +0900

    Fix quasar default breakpoint

--- counter-pinia/src/quasar-user-options.ts
+++ counter-pinia/src/quasar-user-options.ts
@@ -1,10 +1,6 @@
-
-import './styles/quasar.scss'
-import '@quasar/extras/material-icons/material-icons.css'
-
-// To be used on app.use(Quasar, { ... })
+import "./styles/quasar.scss";
+import "@quasar/extras/material-icons/material-icons.css";
 export default {
   config: {},
-  plugins: {
-  }
-}
+  plugins: {},
+};
