b8bea30bbd704953c490b2d2050872d7a88c098e
seona-shim <hogugu4102@codebrick.co>
2022-05-17 17:52:48 +0900

    Design drag and drop component

--- counter-pinia/src/components/default/dnd/CounterDnd.vue
+++ counter-pinia/src/components/default/dnd/CounterDnd.vue
@@ -7,18 +7,26 @@
       item-key="id"
       handle=".handle"
       @change="changeList"
+      class="dnd-box"
     >
       <template #item="{ element, index }">
-        <div class="dnd-item">
+        <div class="dnd-item" :math="element.math">
           <div class="handle"><span /><span /><span /></div>
-          <MathSelector v-model="items[index].math" />
-          <input type="number" v-model="element.count" />
-          <button @click="deleteItem(index)">delete</button>
+          <div class="dnd-item-inner">
+            <MathSelector
+              v-model="items[index].math"
+              @change="changeMath(element.math, element.count, index)"
+            />
+            <input type="number" v-model="element.count" />
+            <button @click="deleteItem(index)">delete</button>
+          </div>
         </div>
       </template>
     </draggable>
-    <button @click="addItem">Add</button>
-    <button @click="runDndCounter">Run</button>
+    <div class="button-box">
+      <button @click="addItem">Add Card</button>
+      <button @click="runDndCounter">Run</button>
+    </div>
   </section>
 </template>
 
@@ -66,33 +74,105 @@ const counterStore = useCounterStore();
 const runDndCounter = () => {
   counterStore.addDndResult(items.value);
 };
+
+const isDivision = (math: string) => {
+  return math == "/" || math == "%";
+};
+
+const changeMath = (math: string, count: number, index: number) => {
+  isDivision(math) && !count ? (items.value[index].count = 1) : null;
+};
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .count-dnd {
-  background: pink;
-  overflow: scroll;
-}
-.handle {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 5px;
-  width: 40px;
-  height: 80px;
-  cursor: grab;
-  span {
-    display: block;
-    width: 5px;
-    height: 5px;
-    background: black;
-    border-radius: 100%;
+  button {
+    opacity: 0.8;
+    &:hover {
+      opacity: 1;
+    }
   }
 }
 .dnd-item {
+  --theme-color: lightblue;
+  &[math="+"] {
+    --theme-color: lightblue;
+  }
+  &[math="-"] {
+    --theme-color: lightpink;
+  }
+  &[math="/"] {
+    --theme-color: lightgreen;
+  }
+  &[math="%"] {
+    --theme-color: plum;
+  }
+  &[math="*"] {
+    --theme-color: orange;
+  }
+
   background: white;
-  border: 1px solid black;
+  border: 1px solid var(--theme-color);
+  box-shadow: 3px 3px 1px var(--theme-color);
   display: flex;
+  border-radius: 10px;
+  overflow: hidden;
+
+  &:not(:first-child) {
+    margin-top: 10px;
+  }
+
+  .handle {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    width: 40px;
+    height: 80px;
+    cursor: grab;
+
+    span {
+      display: block;
+      width: 5px;
+      height: 5px;
+      background: var(--theme-color);
+      border-radius: 100%;
+    }
+  }
+  .dnd-item-inner {
+    display: grid;
+    width: 100%;
+    grid-template-columns: 1fr 2fr auto;
+    gap: 20px;
+    padding: 20px;
+
+    button {
+      min-width: 80px;
+    }
+    select,
+    input {
+      border: 1px solid var(--theme-color);
+      padding: 0 20px;
+    }
+  }
+}
+
+.button-box {
+  margin-top: 20px;
+  button {
+    min-width: 100px;
+    min-height: 40px;
+    border-radius: 5px;
+    background: rgb(130, 243, 196);
+    border: 1px solid seagreen;
+    color: rgb(19, 119, 139);
+    &:first-child {
+      margin-right: 10px;
+      background: pink;
+      border: 1px solid hotpink;
+      color: rgb(165, 0, 88);
+    }
+  }
 }
 </style>
